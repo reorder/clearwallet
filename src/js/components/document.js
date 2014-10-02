c90625fa6e6dd38ba47221f7ec5503921243292d
BtcDrak <btcdrak@gmail.com>
2014-10-02 19:29:54 +0000

    initial

--- src/js/components/document.js
+++ src/js/components/document.js
@@ -8,6 +8,10 @@ function DocumentViewModel() {
     self.documentTransferModal.show(doc.hash_string, doc.owner)
   }
 
+  self.verifyDocument = function(doc){
+    self.verifyDocumentModal.show()
+  }
+
   self.displayDocumentState = function(data){
 
     docs = []
@@ -49,6 +53,9 @@ function DocumentViewModel() {
   self.init = function() {
     self.documentTransferModal = new DocumentTransferModal()
     ko.applyBindings(self.documentTransferModal, document.getElementById("transferDocumentModal"));
+
+    self.verifyDocumentModal = new VerifyDocumentModal()
+    ko.applyBindings(self.verifyDocumentModal, document.getElementById("verifyDocumentModal"));
 
     failoverAPI('get_documents_for', {'addresses': WALLET.getAddressesList()}, self.displayDocuments);
     failoverAPI('get_document_state_for', {'addresses': WALLET.getAddressesList()}, self.displayDocumentState);
@@ -95,7 +102,7 @@ function DocumentTransferModal() {
       hash_type: 0
     }
 
-    WALLET.doTransaction(self.source(), "create_notary_transfer", options,
+    multiAPI('get_document_for', {'document_hash': self.documentHash()},
                          function(txHash, data, endpoint, addressType, armoryUTx) {
                            var message = "Your document with hash <b class='notoAssetColor'>" + self.documentHash().substring(0,16)+"..." + " </b>will be transferred to " + self.destination() + "."
                            WALLET.showTransactionCompleteDialog(message + ACTION_PENDING_NOTICE, message, armoryUTx);
@@ -114,5 +121,86 @@ function DocumentTransferModal() {
 
     //data entry is valid...submit to the server
     $('#transferDocumentModal form').submit();
+  }
+}
+
+function VerifyDocumentModal() {
+  var self = this;
+
+  self.shown = ko.observable(false);
+  self.documentHash = ko.observable("").extend({required: true});
+
+  self.validationModel = ko.validatedObservable({
+    documentHash: self.documentHash
+  });
+
+  self.resetForm = function() {
+    self.documentHash("");
+    self.validationModel.errors.showAllMessages(false);
+  }
+
+  self.show = function() {
+    self.resetForm();
+    self.shown(true);
+  }
+  self.hide = function() {
+    self.shown(false);
+  }
+
+  self.doAction = function(){
+    options = { source: self.address(),
+         hash_string: self.documentHash(),
+         hash_type: 0
+     }
+    WALLET.doTransaction(self.address(), "create_notary", options,
+       function(txHash, data, endpoint, addressType, armoryUTx) {
+         var message = "Your document with hash <b class='notoAssetColor'>" + self.documentHash() + "</b> "
+         + (armoryUTx ? "will be created" : "has been created") + ".<br/><br/>"
+         + "It will automatically appear under the appropriate address once the network"
+         + " has confirmed it.";
+         WALLET.showTransactionCompleteDialog(message + ACTION_PENDING_NOTICE, message, armoryUTx);
+         self.resetForm();
+       }
+    );
+    self.shown(false);
+  }
+
+  self.doHash = function(f,event){
+    var f = event.target.files[0];
+
+    handleFileSelect(f);
+
+    var progress = function(p) {
+      var w = ((p*100).toFixed(0));
+    }
+
+    var  finished = function(result) {
+      self.documentHash(result.toString(CryptoJSH.enc.Hex))
+    }
+
+    function handleFileSelect(f) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var data = e.target.result;
+        setTimeout(function() {
+          var a = CryptoJSH.SHA256(data,progress,finished);
+        }, 200);
+      };
+      reader.onprogress = function(evt) {
+        if (evt.lengthComputable) {
+          var w = (((evt.loaded / evt.total)*100).toFixed(2));
+        }
+      }
+      reader.readAsBinaryString(f);
+    }
+  }
+  self.submitForm = function() {
+    if (!self.validationModel.isValid()) {
+      self.validationModel.errors.showAllMessages();
+      return false;
+    }
+
+    //data entry is valid...submit to the server
+    $('#verifyDocumentModal form').submit();
   }
 }
